75472c1fa58d746b07990188b9ab3ffa24ea6359
yseij <r0790156@student.thomasmore.be>
2022-02-04 18:05:16 +0100

    Toevoegen van static site generation

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
-function HomePage(){
-    const featuredEvents = getFeaturedEvents();
-    return <div>
-        <ul>
-            <EventList items={featuredEvents}/>
-        </ul>
+
+function HomePage(props) {
+  return (
+    <div>
+      <ul>
+        <EventList items={props.events} />
+      </ul>
     </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
