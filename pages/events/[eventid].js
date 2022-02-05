0c1020cd4490eab7a9ca742bfe84d66b52e55637
yseij <r0790156@student.thomasmore.be>
2022-02-05 18:47:56 +0100

    Gegevens en paden laden voor dynamische pagina's

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -1,19 +1,13 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
 
-import { getEventById } from "../../dummy-data";
+import { getEventById, getAllEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAllert from "../../components/ui/error-alert";
 
-function EventDetailPage() {
-  const router = useRouter();
-
-  router.query.eventId;
-
-  const eventId = router.query.eventid;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -37,6 +31,31 @@ function EventDetailPage() {
       </EventContent>
     </Fragment>
   );
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+
+  const paths = events.map((event) => ({
+    params: { eventId: event.id },
+  }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
 }
 
 export default EventDetailPage;
